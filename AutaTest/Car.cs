c5ac86ffe8980345840da647d6deaa63694478c9
Jakub Vančata <jakub.vancata@gentlecodes.com>
2017-11-07 08:42:18 +0100

    Init commit

---
+++ AutaTest/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutaTest
+{
+    class Car
+    {
+        private string color;
+        private int mileage;
+        private string dateMade;
+        private bool isMoving;
+
+        public Car (string color, int mileage, string dateMade, bool isMoving)
+        {
+            this.color = color;
+            this.mileage = mileage;
+            this.dateMade = dateMade;
+            this.isMoving = isMoving;
+        }
+        //přístupová rozhraní
+        #region
+        public string Color
+        {
+            get {
+                return this.color;
+            }
+            set {
+                this.color = value;
+            }
+        }
+
+        public int Mileage
+        {
+            get
+            {
+                return this.mileage;
+            }
+            set
+            {
+                this.mileage = value;
+            }
+        }
+
+        public string DateMade
+        {
+            get
+            {
+                return this.dateMade;
+            }
+            set
+            {
+                this.dateMade = value;
+            }
+        }
+
+        public bool IsMoving
+        {
+            get
+            {
+                return this.isMoving;
+            }
+            set
+            {
+                this.isMoving = value;
+            }
+        }
+        #endregion
+        public override string ToString()
+        {
+            string moving;
+            if (this.isMoving)
+            {
+                moving = "hýbe";
+            }
+            else
+            {
+                moving = "nehýbe";
+            }
+            return "Vypisuješ class Auto. Toto konkrétní auto má barvu " + this.color + ". Má najeto " + this.mileage + " kilometrů a právě se "+moving+". Bylo vyrobeno v roce " + this.dateMade;
+        }
+    }
+}
--- AutaTest/Program.cs
+++ AutaTest/Program.cs
@@ -4,12 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AutaTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            List<Car> carsList = new List<Car>();
+            List<String> colors = new List<String>()
+            {
+                "hnědá", "béžová", "černá", "šedá", "tmavě šedá", "fakt hodně tmavě šedá"
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                bool moving;
+                int rndMoving = rnd.Next(1, 3);
+                if (rndMoving == 1)
+                {
+                    moving = true;
+                }
+                else
+                {
+                    moving = false;
+                }
+                carsList.Add(new Car(colors[rnd.Next(0, colors.Count() - 1)], rnd.Next(0, 500000), rnd.Next(1980, 2018).ToString(), moving));
+            }
+            //Car car = new Car("hněď", 69, "1999", true);
+
+            int highest = -1;
+            int highestPos = 0;
+            for (int i = 0; i < carsList.Count()-1; i++)
+            {
+                if (carsList[i].Mileage > highest)
+                {
+                    highest = carsList[i].Mileage;
+                    highestPos = i;
+                }
+                Console.WriteLine("Vypisuji " + i + ". auto");
+                Console.WriteLine(carsList[i]+"\n");
+            }
+            Console.WriteLine("Nejvíce má najeto toto auto: ");
+            Console.WriteLine(carsList[highestPos]);
+            
         }
     }
 }
